696b0e0ebb9df89e3285d342f490c89780fe6704
Kevin VanderLugt <kevin.vanderlugt@gmail.com>
2013-09-12 22:12:28 -0700

    Add ability to create a new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,17 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "User was created"
+      redirect_to users_url
+    else
+      render 'new'
+    end
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :name)
+    end
 end
