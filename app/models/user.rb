2dd27f121b9cd20aa1f6a5d8b948c1ff20f6528b
Kevin VanderLugt <kevin.vanderlugt@gmail.com>
2013-09-13 13:16:15 -0700

    Normalize the user email to downcase

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,12 @@ class User < ActiveRecord::Base
 
   validates :email, uniqueness: true, format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/
   validates :name, presence: true
+
+  before_save :normalize_email
+
+  private
+    def normalize_email
+      # There is probably more to normalizing an email but I am not entirely sure what it should be
+      state.downcase!
+    end
 end
