b077168a49ea0725fff612e21cdf85216cfe2033
Kevin VanderLugt <kevin.vanderlugt@gmail.com>
2013-09-12 22:28:43 -0700

    Add validations for license state to only be 2 letters

--- app/models/license.rb
+++ app/models/license.rb
@@ -1,2 +1,4 @@
 class License < ActiveRecord::Base
+  validates :state, length: { is: 2 }, format: { with: /\A[a-zA-Z]+\z/,
+    message: "only letters" }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130913032354) do
+ActiveRecord::Schema.define(version: 20130913052449) do
+
+  create_table "licenses", force: true do |t|
+    t.string   "number"
+    t.string   "state"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "name"
