a5a6ec963274beead1105a0d853d0133dd03ec05
Kevin VanderLugt <kevin.vanderlugt@gmail.com>
2013-09-13 13:14:49 -0700

    Add licenses to the user index page and allows nested licenses to the whitelist

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,6 @@ class UsersController < ApplicationController
 
   private
     def user_params
-      params.require(:user).permit(:email, :name)
+      params.require(:user).permit(:email, :name, licenses_attributes: [:number, :state, :id])
     end
 end
--- app/models/license.rb
+++ app/models/license.rb
@@ -2,5 +2,16 @@ class License < ActiveRecord::Base
   validates :state, length: { is: 2 }, format: { with: /\A[a-zA-Z]+\z/,
     message: "only letters" }
   belongs_to :user
-  validates :user_id, presence: true
+  #validates :user_id, presence: true
+
+  before_save :normalize_state
+
+  def to_s
+    "#{number}, #{state}"
+  end
+
+  private
+    def normalize_state
+      state.upcase!
+    end
 end
