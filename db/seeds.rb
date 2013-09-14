0548902b29a14103babbed772c6281a1e4564281
Kevin VanderLugt <kevin.vanderlugt@gmail.com>
2013-09-13 19:44:07 -0700

    Adds database seeds for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+u = User.create({name: "Kevin VanderLugt", email: "kevin.vanderlugt@gmail.com"})
+u.licenses.create({state: "WA", number:"1111"})
+
+u = User.create({name: "Bob Jones", email: "bjones@gmail.com"})
+u.licenses.create({state: "WA", number:"11231"})
+
+u = User.create({name: "Some dude", email: "some.dude@gmail.com"})
+u.licenses.create({state: "OR", number:"1111"})
+
+u = User.create({name: "Another guy", email: "ag@gmail.com"})
+u.licenses.create({state: "WA", number:"1111"})
+
+u = User.create({name: "hotmail dude", email: "hotmaildude@hotmail.com"})
+u.licenses.create({state: "FL", number:"22222"})
